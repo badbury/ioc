8945fea52c0d175503c28f1802e64694b6c85db1
Simon Holloway <simon@syhol.io>
2021-04-21 01:15:24 +0100

    rename

---
+++ src/listeners.ts
@@ -0,0 +1,57 @@
+import { Container, Listener } from './container';
+
+type Constructor<T = any, P extends any[] = any[]> = new (...args: P) => T;
+type ClassLike<T extends Constructor<T>> = Constructor<InstanceType<T>>;
+
+type MethodOf<
+  TClassType extends Constructor,
+  TSubjectType extends Constructor,
+  TClass = InstanceType<TClassType>,
+  TSubject = InstanceType<TSubjectType>
+> = {
+  [TProperty in keyof TClass]: TClass[TProperty] extends (arg: infer U) => any
+    ? U extends TSubject
+      ? TProperty
+      : never
+    : never;
+}[keyof TClass];
+
+export class EventListenerBuilder<T extends ClassLike<T>> {
+  constructor(public key: T) {}
+
+  do(target: (subject: InstanceType<T>) => void): Listener<T>;
+  do<C extends Constructor, M extends MethodOf<C, T>>(target: C, method: M): Listener<T>;
+  do<C extends Constructor, M extends MethodOf<C, T>>(
+    target: C | ((subject: InstanceType<T>) => void),
+    method?: M,
+  ): Listener<T> {
+    return method
+      ? new ClassEventListener(this.key, target as C, method)
+      : new FunctionEventListener(this.key, target as (subject: InstanceType<T>) => void);
+  }
+}
+
+export class ClassEventListener<
+  T extends ClassLike<T>,
+  V extends Constructor,
+  M extends MethodOf<V, T>
+> extends Listener<T> {
+  constructor(public key: T, private listenerClass: V, private listenerMethod: M) {
+    super(key);
+  }
+
+  handle(subject: InstanceType<T>, container: Container): void {
+    const handler = container.get(this.listenerClass);
+    handler[this.listenerMethod](subject);
+  }
+}
+
+export class FunctionEventListener<T extends ClassLike<T>> extends Listener<T> {
+  constructor(public key: T, private handler: (subject: InstanceType<T>) => any) {
+    super(key);
+  }
+
+  handle(subject: InstanceType<T>): void {
+    this.handler(subject);
+  }
+}
---
+++ src/resolvers.ts
@@ -0,0 +1,93 @@
+import { Container, Resolver } from './container';
+
+export type PartialResolver<
+  T extends new (...args: any[]) => InstanceType<T>,
+  P extends TypeParams<T>
+> = {
+  with(...args: P): Resolver<T>;
+};
+
+type AsConstructors<T extends any[]> = {
+  [K in keyof T]: (new (...args: any[]) => T[K]) | Resolver<T[K]>;
+};
+
+type TypeParams<T extends new (...args: any[]) => any> = T extends new (...args: infer P) => any
+  ? AsConstructors<P>
+  : never;
+
+export class BindResolver<
+    T extends new (...args: any[]) => InstanceType<T>,
+    K = T,
+    P extends TypeParams<T> = TypeParams<T>
+  >
+  extends Resolver<T, K>
+  implements PartialResolver<T, P> {
+  protected instance?: InstanceType<T>;
+
+  constructor(public key: K, public type: T, private args: P | [] = []) {
+    super(key);
+  }
+
+  resolve(container: Container): T {
+    if (this.instance) {
+      return this.instance;
+    }
+    const args = [];
+    for (const arg of this.args) {
+      args.push(arg instanceof Resolver ? arg.resolve(container) : container.get(arg));
+    }
+    return (this.instance = new this.type(...args));
+  }
+
+  with(...args: P): BindResolver<T, K, P> {
+    return new BindResolver(this.key, this.type, args);
+  }
+
+  to<N>(key: N): BindResolver<T, N, P> {
+    return new BindResolver(key, this.type, this.args);
+  }
+}
+
+export class TransformResolver<
+  T extends new (...args: any[]) => InstanceType<T>,
+  N,
+  K
+> extends Resolver<N> {
+  private instance?: N;
+
+  constructor(
+    public key: K,
+    private next: Resolver<T>,
+    private transform: (t: InstanceType<T>) => N,
+  ) {
+    super(key);
+  }
+
+  resolve(container: Container): N {
+    if (this.instance) {
+      return this.instance;
+    }
+    const instance = this.next.resolve(container);
+    return (this.instance = this.transform(instance as any));
+  }
+}
+
+export class LookupResolver<T extends new (...args: any[]) => InstanceType<T>> extends Resolver<T> {
+  resolve(container: Container): T {
+    return container.get(this.key);
+  }
+
+  map<N>(transform: (t: InstanceType<T>) => N): Resolver<N> {
+    return new TransformResolver(this.key, this, transform);
+  }
+}
+
+export class ValueResolver<T, K> extends Resolver<T, K> {
+  constructor(public key: K, private value: T) {
+    super(key);
+  }
+
+  resolve(): T {
+    return this.value;
+  }
+}
